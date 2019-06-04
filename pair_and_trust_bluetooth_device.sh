09889c44d0d01ce28dec8179a81657e095ff07ca
rocheparadox <rocheinside@gmail.com>
2019-06-04 17:50:56 +0100

    tweaks

--- pair_and_trust_bluetooth_device.sh
+++ pair_and_trust_bluetooth_device.sh
@@ -1,12 +1,15 @@
 cd $(dirname $0)
 echo "Pairing..."
 expect pair_bluetooth_device.expect > expect_script.log
-
+chmod 777 expect_script.log
 sleep 2
 
 echo "Trusting and connecting.."
-device_mac_address=$(cat expect_script.log | grep -Pom 1 "(?<=Device ).*(?= UUID)")
+device_mac_address=$(cat expect_script.log | grep -Pom 1 "(?<=Device ).*(?= Connected)")
 echo mac address is $device_mac_address
-expect trust_and_connect.expect $device_mac_address
-
+if [[ ! -z $device_mac_address ]] ; then
+	expect trust_and_connect.expect $device_mac_address
+else
+	echo "No device connected"
+fi
 rm expect_script.log
